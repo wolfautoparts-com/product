2592b4f7e06c916ddec3802449ee3c80da22723f
Dmitry Fedyuk <dfediuk@gmail.com>
2019-09-06 19:24:20 +0600

    0.0.1

---
+++ Block/Fitment.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Wolf\Product\Block;
+
+/**
+*
+*/
+class Fitment extends \Magento\Framework\View\Element\Template
+{
+    function getTitle()
+    {
+        return "Wolf Product Fitment";
+    }
+}
